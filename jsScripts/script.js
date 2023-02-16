8614a52377a9abd2cf6d8aad7891f85aeef9eb44
Plato <plato.charneski@gmail.com>
2023-02-16 08:44:39 -0500

    Creatures Of Night logo

--- jsScripts/script.js
+++ jsScripts/script.js
@@ -1,14 +1,14 @@
-function myFunction(text) {
-    // Get the text field
-    var copyText = document.getElementById("copyText");
-  
-    // Select the text field
-    //copyText.select();
-    //copyText.setSelectionRange(0, 99999); // For mobile devices
-  
-     // Copy the text inside the text field
-    navigator.clipboard.writeText(text);
-  
-    // Alert the copied text
-    alert("Copied Text!");
-  }
+function myFunction() {
+  // Get the text field
+  var copyText = document.getElementById("myInput");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+  // Alert the copied text
+  alert("Copied the text: " + copyText.value);
+}
